e2e73b4104a084d52d9fccd295fa584db21ad033
kemchuoihot <thinhpx33@gmail.com>
2024-06-19 12:36:12 +0700

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -54,12 +54,12 @@ mongoose.connection.on("connected", () => {
 app.get('/', (req, res) =>
   res.send(a)
 );
-app.use('/', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use('/', (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 
 
