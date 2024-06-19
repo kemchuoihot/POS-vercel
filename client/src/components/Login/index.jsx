3da24e57a5bc16bc44e819e3b2b77080426a682b
kemchuoihot <thinhpx33@gmail.com>
2024-06-19 12:19:55 +0700

    Update index.jsx

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -30,7 +30,7 @@ const Signup = () => {
         setLoading(true);
         e.preventDefault();
         try {
-            const response = await axios.post("pos-vercel-server.vercel.app/auth", {username,password})
+            const response = await axios.post("pos-vercel-server-kac1nbmpr-kindys-projects-0a9d1e8f.vercel.app/auth", {username,password})
             .then((result) => {
                 console.log(result);
                 const res = result.data;
